3fa173ea760c7e20e4cd68301bd34322b039af78
Choon Kee Oh <choonkee@andLinux.(none)>
2010-01-05 16:30:08 +0800

    Project Creation

---
+++ src/com/appspot/demo/paypal/server/GreetingServiceImpl.java
@@ -0,0 +1,19 @@
+package com.appspot.demo.paypal.server;
+
+import com.appspot.demo.paypal.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+		GreetingService {
+
+	public String greetServer(String input) {
+		String serverInfo = getServletContext().getServerInfo();
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+		return "Hello, " + input + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+}
